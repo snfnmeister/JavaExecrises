c871b8dfcd1255597066ca54bae101b17603fb4c
snfnmeister <igorizhenbin@gmail.com>
2019-08-24 11:26:24 +0700

    Add System.out.println logic

--- InputOutput/SystemOut.java
+++ InputOutput/SystemOut.java
@@ -1,2 +1,32 @@
+
+/*
+ * How Java works with output messages
+ *
+ */
+
 public class SystemOut {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello World!");
+
+        int a = 14;
+        int b = 88;
+        double c = (double) a / b;
+
+        System.out.println(a + b);
+
+        System.out.println("Variable a contains " + a);
+
+        System.out.println("Variable c equal to = " + (double) a / b);
+
+        System.out.println("Variable c equal to = " + c);
+
+        System.out.println(c);
+
+        System.out.print("c = ");
+        System.out.println(c);
+
+
+    }
 }
