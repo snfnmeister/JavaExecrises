7e4076d2bbbd6a9f876460749b8695226666708e
snfnmeister <igorizhenbin@gmail.com>
2019-08-24 11:43:38 +0700

    Add Primitive types discription

---
+++ DataTypes/MainTypes.java
@@ -0,0 +1,16 @@
+/*
+* Java Primitive Types
+*
+* byte (integer numbers, 1 byte size, range -128..127 );
+* short (integer numbers, 2 byte size, range -32768..32767);
+* int (integer numbers, 4 byte size, range -2147483648..2147483647);
+* long (integer numbers, 8 bytes size, range -9223372036854775808..9223372036854775807);
+* float (real numbers, 4 bytes size, range ~1,4*10-45..~3,4*1038);
+* double (real numbers, 8 bytes size, range ~4,9*10-324..~1,8*10308);
+* char (Unicode symbol, 2 bytes size, chars with equal number range 0..65535)
+* boolean (value true/false, 1 bytes size, range 0..1, 1 for true, 0 for false)
+*
+*/
+
+public class MainTypes {
+}
