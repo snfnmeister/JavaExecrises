a6710210d285c3a467b22f6099ee9a31dd8bec45
snfnmeister <igorizhenbin@gmail.com>
2019-08-30 17:50:49 +0700

    Add comparison operators

--- Operators/ComparisonOperators.java
+++ Operators/ComparisonOperators.java
@@ -1,2 +1,24 @@
-public class ComparisonOperators {
+/*
+ *
+ * Java Comparison Operators
+ *
+ */
+
+class ComparisonOperators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Оператор '==' - для определения равенства: a == b? Ответ: " + (a == b));
+        System.out.println("Оператор '!=' - для определения неравенства: a != b? Ответ: " + (a != b));
+        System.out.println("Оператор '>' -  определение a больше b: a > b? Ответ: " + (a > b));
+        System.out.println("Оператор '<' -  определение a меньше b: a < b? Ответ: " + (a < b));
+        System.out.println("Оператор '>=' -  определение a больше или равно b: a >= b? Ответ: " + (a >= b));
+        System.out.println("Оператор '<=' -  определение a меньше или равно b: a <= b? Ответ: " + (a <= b));
+
+    }
+
+
 }
