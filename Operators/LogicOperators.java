754bcd0f09e1badc8fea42983017c9f6e8e43f0b
snfnmeister <igorizhenbin@gmail.com>
2019-09-01 09:32:21 +0700

    add LogicOperators

--- Operators/LogicOperators.java
+++ Operators/LogicOperators.java
@@ -1,2 +1,19 @@
+/*
+ * Java logic operators
+ *
+ * && - AND (shortcut)
+ * || - OR (shortcut)
+ * ! - NOT
+ * */
+
 public class LogicOperators {
+
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+
+        System.out.println("a AND b = " + (a && b));
+        System.out.println("a OR b = " + (a || b));
+        System.out.println("NOT(a AND b) = " + !(a && b));
+    }
 }
