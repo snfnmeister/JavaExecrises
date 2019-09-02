74254aac3059a2b3fa7ecf4a4939ca9ee6eccb52
snfnmeister <igorizhenbin@gmail.com>
2019-09-02 18:02:53 +0700

    Add Truth Table

--- Operators/TruthTable.java
+++ Operators/TruthTable.java
@@ -1,9 +1,26 @@
 /*
-* Make Truth Table for java
-* */
+ * Make Truth Table for java
+ * */
 
 public class TruthTable {
     public static void main(String[] args) {
+
+
+        boolean[] c = {false, true, false, true};
+        boolean[] d = {false, false, true, true};
+
+        System.out.println("p    |q    |p & q|p | q|p ^ q|!p");
+
+        for (int i = 0; i < 4; i++) {
+            boolean a = c[i];
+            boolean b = d[i];
+
+            System.out.println(a + "|" + b + "|" + (a & b) + "|" +
+                    (a | b) + "|" + (a ^ b) + "|" + (!a));
+
+
+        }
+
 
     }
 }
