9f80d2a2847feedfa8f3a4aa523c88b3955ba838
snfnmeister <igorizhenbin@gmail.com>
2019-09-12 14:58:56 +0700

    changed a assignment operators a bit

--- Operators/AssignmentOperators.java
+++ Operators/AssignmentOperators.java
@@ -12,6 +12,7 @@ public class AssignmentOperators {
         float A = 10F;
         float B = 20F;
         float C = 0;
+        int D = -4;
 
         System.out.println("Оператор присваивания '=': C = A + B - " + (C = (A + B)));
         System.out.println("Оператор +=: C += A эквивалентно C = C + A = " + (C += A));
@@ -19,6 +20,10 @@ public class AssignmentOperators {
         System.out.println("Оператор *=: C *= A эквивалентно C = C * A = " + (C *= A));
         System.out.println("Оператор /=: C /= B эквивалентно C = C / B = " + (C /= B));
         System.out.println("Оператор %=: C %= A эквивалентно C = C % A = " + (C %= 4));
+        System.out.println("Оператор >>=: D >>= 1 эквивалетно D = D >> 1 = " + (D >>= 1)); //value divided by 2
+        System.out.println("Оператор <<=: D <<= 1 эквивалетно D = D << 1 = " + (D <<= 1)); //value multiplied by 2
+        System.out.println("Оператор >>>=: D >>>= 1 эквивалетно D = D >>> 1 = " + (D >>>= 1)); //div by 2 + high zero bit
+
 
     }
 }
