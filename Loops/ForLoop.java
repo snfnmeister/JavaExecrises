847aee49629623212bddeb23a2eacec38046814e
snfnmeister <igorizhenbin@gmail.com>
2019-09-21 23:29:30 +0700

    add for loop

---
+++ Loops/ForLoop.java
@@ -0,0 +1,18 @@
+/*
+ *
+ * Java For Loop
+ * syntax:  for (initialization; termination;
+ *               increment) {
+ *               statement(s)
+ *           }
+ */
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i is equal to " + i);
+        }
+
+    }
+}
