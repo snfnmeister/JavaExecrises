3d6c6265bcaa70e8986850ebc10f7f921a395c00
snfnmeister <igorizhenbin@gmail.com>
2019-10-31 14:53:00 +0700

    added ArrayOwnReverse - way how to reverse array (w/o adding new array)

---
+++ Strings/StringsAbbreviations.java
@@ -0,0 +1,9 @@
+/*
+ *
+ * Write a function to convert a name into initials. Takes two words (maybe more) with one space (maybe more)
+ * in between them and convert to Abbreviations. If input string have 2 words - return string in "A.B" format.
+ * If string contains more than 2 words return usual abbreviations
+ *
+ */
+public class StringsAbbreviations {
+}
