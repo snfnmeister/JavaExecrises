27b70465d6a326e6667767383f8b50e14286b354
snfnmeister <igorizhenbin@gmail.com>
2019-11-04 12:42:27 +0700

    added some Temp

---
+++ Temp/SayMyName.java
@@ -0,0 +1,30 @@
+public class SayMyName {
+
+    public static void main(String[] args) {
+
+        String I = "I\nI\nI\nI\nI";
+        String G = " \nGGGGG" +
+                "\nG     G" +
+                "\nG     " +
+                "\nG    GGG" +
+                "\nG     G" +
+                "\n GGGGG";
+        String O = "\n00000 " +
+                "\n0   0 " +
+                "\n0   0 " +
+                "\n00000 ";
+        String R = "\nRRRR" +
+                "\nR   R" +
+                "\nRRR" +
+                "\nR  R" +
+                "\nR   R";
+
+
+//        System.out.println(I);
+//        System.out.println(G);
+//          System.out.println(O);
+//        System.out.println(R);
+        System.out.print(I + "  " + G + "  " + O + "  " + R + "  ");
+
+    }
+}
