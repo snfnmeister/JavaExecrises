df23d636b1ce7730d82c8ba7c3615efe77c41720
snfnmeister <igorizhenbin@gmail.com>
2019-11-12 17:36:51 +0700

    added System.out.println(args)

---
+++ Temp/Args.java
@@ -0,0 +1,12 @@
+/*
+ *
+ * System.out.println(args)
+ *
+ */
+public class Args {
+
+    public static void main(String[] args) {
+
+        System.out.println("args - " + args.length + "\t" + args);
+    }
+}
