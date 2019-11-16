74f274741b56adc0695541eaecc527c23b2acccb
snfnmeister <igorizhenbin@gmail.com>
2019-11-16 13:39:26 +0700

    added Calculator methods() return

--- OOP/Calculator.java
+++ OOP/Calculator.java
@@ -9,26 +9,26 @@ public class Calculator {
     int b;
 
     //summ method
-    void summ() {
-        System.out.println("summ is " + (a + b));
+    int summ() {
+        return a + b;
     }
 
     //substraction method
-    void substract() {
-        System.out.println("substraction is " + (a - b));
+    int substract() {
+        return a - b;
     }
 
     //divide method
-    void divide() {
+    int divide() {
         if (b != 0) {
-            System.out.println("divide is " + (a / b));
+            return a / b;
         } else {
-            System.out.println("b must be greater then 0");
+            return 0;
         }
     }
 
     //multiply method
-    void multiply() {
-        System.out.println("multiply is " + (a * b));
+    int multiply() {
+        return a * b;
     }
 }
--- OOP/CalculatorDemo.java
+++ OOP/CalculatorDemo.java
@@ -11,13 +11,15 @@ public class CalculatorDemo {
         Calculator math = new Calculator();
 
         math.a = 20;
-        math.b = 0;
+        math.b = 5;
 
         //use methods Calculator class
 
-        math.summ();
-        math.substract();
-        math.multiply();
-        math.divide();
+        System.out.println("a + b = " + math.summ());
+        System.out.println("a - b = " + math.substract());
+        System.out.println("a * b = " + math.multiply());
+        System.out.println("a / b = " + math.divide());
+
+
     }
 }
