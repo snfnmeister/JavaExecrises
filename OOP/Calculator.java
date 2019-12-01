5c0614327331466c2f2479186a2c475a6234b8c2
snfnmeister <igorizhenbin@gmail.com>
2019-12-01 10:51:15 +0700

    added override for methods and constructors of Calculator class

--- OOP/Calculator.java
+++ OOP/Calculator.java
@@ -1,40 +1,80 @@
 /*
  *
- * Make a calculator class with own methods (summ, substraction, divide, multiply);
+ * Make a calculator class with own methods (summ, subtraction, divide, multiply);
  *
  */
 
-public class Calculator {
-    int a;
-    int b;
+class Calculator {
+    private int a;
+    private int b;
+    private double c;
+    private double d;
 
     //constructor for Calculator class
-    public Calculator(int a, int b) {
+    Calculator(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
+    Calculator(double c, double d) {
+        this.c = c;
+        this.d = d;
+    }
+
+    void getIntData() {
+        System.out.println("a = " + a + "\nb = " + b);
+    }
+
+    void getDoubleData() {
+        System.out.println("c = " + c + "\nd = " + d);
+    }
+
+
     //summ method
-    int summ() {
+    int summ(int a, int b) { //all other not float data types will typecasts to int
         return a + b;
     }
 
+    double summ(double a, double b) { //float data will cast to double
+        return a + b;
+    }
+
+
     //substraction method
-    int substract() {
+    int substract(int a, int b) {
+        return a - b;
+    }
+
+    double substract(double a, double b) {
         return a - b;
     }
 
     //divide method
-    float divide() {
+    int divide(int a, int b) {
         if (b != 0) {
-            return (float) a / b;
+            return a / b;
+        } else {
+            return 0;
+        }
+    }
+
+    //divide method
+    double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
         } else {
             return 0;
         }
     }
 
     //multiply method
-    int multiply() {
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //multiply method
+    double multiply(double a, double b) {
         return a * b;
     }
 }
+
--- OOP/CalculatorDemo.java
+++ OOP/CalculatorDemo.java
@@ -9,16 +9,31 @@ public class CalculatorDemo {
     public static void main(String[] args) {
 
         int x = 1;
-        int y = 1000;
+        int y = 2;
+        double c = 13.37D;
+        double d = 14.88D;
 
-        Calculator math = new Calculator(20, 5);
-        Calculator mathOne = new Calculator(x, y);
+        Calculator math = new Calculator(x, y);
+        Calculator mathOne = new Calculator(c, d);
+        math.getIntData();
+        mathOne.getDoubleData();
 
         //use methods of Calculator class
-        System.out.println("a + b = " + math.summ());
-        System.out.println("a - b = " + math.substract() + "\n");
-        System.out.println("a * b = " + mathOne.multiply());
-        System.out.println("a / b = " + mathOne.divide());
+
+        System.out.println("SUMM\nInteger (a + b) = " + math.summ(x, y)); //its truly integer?
+        System.out.println("Double (a + b) = " + math.summ(c, d));
+
+        System.out.println("SUBSTRACT\nInteger (a - b) = " + math.substract(x, y));
+        System.out.println("Double (a - b) = " + math.substract(c, d));
+
+        System.out.println("MULTIPLY\nInteger (a * b) = " + mathOne.multiply(x, y));
+        System.out.println("Double (a * b) = " + mathOne.multiply(c, d));
+
+        System.out.println("DIVIDE\nInteger (a / b) = " + mathOne.divide(x, y));
+        System.out.println("Double (a / b) = " + mathOne.divide(c, d));
+
+        //        System.out.println("Integer ('A' + 'B') = " + math.summ('A','B')); //char will autocast to int
+
 
     }
 }
