4949fe347afd1e944392cbb00262c2a778492325
snfnmeister <igorizhenbin@gmail.com>
2019-12-18 17:21:24 +0700

    fixed a bit

--- MethodsAndClassAdds/VarArgsOverloading.java
+++ MethodsAndClassAdds/VarArgsOverloading.java
@@ -9,11 +9,12 @@ public class VarArgsOverloading {
     static void vaTest(int...v) { //argument with variable length must be last in method declaration!
         System.out.println("vaTest(int...v) - number of args = " + v.length);
         System.out.println("Args: ");
+        int j = 0;
 
-        for(int i = 0; i < v.length; i++) {
-            System.out.println(" args" + i + ": " + v[i]);
-        }
-        System.out.print("\n");
+       for(int i : v) {
+           System.out.println(" args" + j + ": " + i);
+           j++;
+       }
     }
 
     // will not work because of uncertainty. Compiler can't choose which method use:
@@ -52,7 +53,7 @@ public class VarArgsOverloading {
 
 
     public static void main(String[] args) {
-        vaTest(1,2,3,4,5,6,7,8,9,10);
+        vaTest(10,20,30,40,50);
         vaTest(false,false,true,false,false);
         vaTest("Hello", 'W','o','r','d','!');
 //        vaTest(); don't work because vaTest() method with empty arguments not specified
