c6fc0766ed6b29c04b7f82d378dcbc7b0b5a4317
snfnmeister <igorizhenbin@gmail.com>
2019-12-24 18:27:05 +0700

    added decision of some task

--- Temp/EmptyConsole.java
+++ Temp/EmptyConsole.java
@@ -1,8 +1,22 @@
 /*
-*
-* Find a way how to clear console log by Java methods
-*
-*/
+ *
+ * Find a way how to clear console log by Java methods
+ *
+ */
 
 public class EmptyConsole {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello world";
+        System.out.print(str1);
+//        System.out.println("World!");
+
+
+        for (int i = 0; i < str1.length(); i++) {
+            System.out.print("\b");
+        }
+
+
+    }
 }
