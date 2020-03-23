11a5416439d92671cee170c7ca1c5e83ba891566
snfnmeister <igorizhenbin@gmail.com>
2020-03-23 13:24:22 +0700

    added some Work Temp

---
+++ Temp/StartingMethod.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Class for starting some method
+ *
+ */
+
+public class StartingMethod {
+    public static void main(String[] args) {
+        ControlTemp temp1 = new ControlTemp();
+        boolean errorStatus = false;
+        errorStatus = temp1.checkError(15, 0);
+        System.out.println(errorStatus);
+    }
+}
