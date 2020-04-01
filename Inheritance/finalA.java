dbb160605159116f9a464575b4adbe0f1ec8fda7
snfnmeister <igorizhenbin@gmail.com>
2020-04-01 15:42:22 +0700

    added examples of using final keywords for methods

---
+++ Inheritance/finalA.java
@@ -0,0 +1,11 @@
+/*
+ *
+ * Example of using final keyword for methods
+ *
+ */
+
+public class finalA {
+    final void meth() {
+        System.out.println("This is final method");
+    }
+}
---
+++ Inheritance/finalB.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Example of using final keyword for methods
+ *
+ */
+
+public class finalB extends finalA {
+    //    void meth() { overriden method is final
+    public static void main(String[] args) {
+
+        System.out.println("Dont' allow!");
+
+    }
+}
